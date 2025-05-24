d704436387ce43892f9226972d80327b7d547619
Aayush Lamichhane <aayushlamichhane2911@gmail.com>
2025-05-24 22:27:45 +0545

    update: change rate limiter and update verify otp schema

--- backend/src/utils/validate/auth/verifyOtpSchema.ts
+++ backend/src/utils/validate/auth/verifyOtpSchema.ts
@@ -3,15 +3,7 @@ import { z } from "zod";
 export const VerifyOtpSchema = z.object({
   code: z.string().length(6, { message: "Invalid otp." }),
   uid: z.string(),
-  type: z.enum(["otp", "reset"]),
-  newPassword: z
-    .string()
-    .min(8, { message: "Password must be atleast 8 character long." })
-    .regex(
-      /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-      "Password must contain an uppercase character, a digit and a special symbol."
-    )
-    .optional(),
+  type: z.enum(["otp", "reset"])
 });
 
 export type VerifyOtpSchemaType = z.infer<typeof VerifyOtpSchema>;
