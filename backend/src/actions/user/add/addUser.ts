54cc1bb60515e653af03f5806dd4a0340d18b90e
Aayush Lamichhane <aayushlamichhane2911@gmail.com>
2025-06-18 21:23:32 +0545

    update: fix changing password when updating role

--- backend/src/actions/user/add/addUser.ts
+++ backend/src/actions/user/add/addUser.ts
@@ -8,7 +8,8 @@ import logger from "../../../utils/logger/logger.js";
 export const addUserToFirestore = async (
   user: Auth.Register,
   access: User.RoleType,
-  uid?: string
+  uid?: string,
+  isUpdatingRole: boolean = false
 ) => {
   const customerDocRef = db.collection(access);
   if (!customerDocRef) throw new APIError("No document found.", 404);
@@ -17,7 +18,10 @@ export const addUserToFirestore = async (
     const oldUser = await customerDocRef.where("email", "==", user.email).get();
     if (oldUser.size !== 0)
       throw new APIError("User already exist with this email.", 409);
-    const hashedPassword = await generateHashedPassword(user.password);
+    let hashedPassword = user.password;
+    if (!isUpdatingRole) {
+      hashedPassword = await generateHashedPassword(user.password);
+    }
     await customerDocRef.doc(userId).set({
       fullName: `${user.firstName?.trim()} ${user.lastName?.trim()}`,
       email: user.email,
